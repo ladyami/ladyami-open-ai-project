6d9297eb4eec6dc598f69f7d42cebf1f8f0ad7f7
ladyami <99187115+ladyami@users.noreply.github.com>
2022-12-24 20:25:02 +0100

    developed the backend and connect it to the front

--- client/script.js
+++ client/script.js
@@ -12,7 +12,7 @@ function loader(element) {
   loadInterval = setInterval(() => {
     element.textContent += ".";
 
-    if (element.textContenxt === "....") {
+    if (element.textContent === "....") {
       element.textContent = "";
     }
   }, 300);
@@ -23,7 +23,7 @@ function typeText(element, text) {
 
   let interval = setInterval(() => {
     if (index < text.length) {
-      element.inner.HTML += text.ChartAt(index);
+      element.innerHTML += text.charAt(index);
       index++;
     } else {
       clearInterval(interval);
@@ -41,7 +41,7 @@ function generateUniqueId() {
 function chatStripe(isAi, value, uniqueId) {
   return `<div class=" wrapper ${isAi && "ai"}"> 
     <div class="chat"> 
-    <div className="profile">
+    <div class="profile">
     <img src="${isAi ? bot : user}" alt="${isAi ? "bot" : "user"}" />
     </div>
     <div class="message" id=${uniqueId}> ${value}</div>
@@ -62,9 +62,33 @@ const handleSubmit = async (e) => {
 
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
-  const massageDiv = document.getElementById(uniqueId);
+  const messageDiv = document.getElementById(uniqueId);
 
-  loader(massageDiv);
+  loader(messageDiv);
+
+  const response = await fetch("http://localhost:5000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "something went wrong ";
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
